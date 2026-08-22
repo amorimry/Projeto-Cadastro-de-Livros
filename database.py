826e7ba5e1809365e73e1de4270446180b2b64e0
Ryan <274507430+amorimry@users.noreply.github.com>
2026-08-22 18:07:27 -0300

    esqueleto do database

---
+++ database.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+ARQUIVO = "Projeto Cadastro de Livros"
+
+def carregar_livros():
+    pass
+
+def salvar_livro():
+    pass
+
+def adicionar_livro():
+    pass
+
+def atualizar_livro():
+    pass
+
+def excluir_livro():
+    pass
