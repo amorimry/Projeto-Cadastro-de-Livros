2d76aae331c1ade45caa12243aab84e8c8bb1f10
amorimry <274507430+amorimry@users.noreply.github.com>
2026-08-24 19:57:34 -0300

    criação da função de editar

--- main.py
+++ main.py
@@ -27,6 +27,61 @@ def adicionar():
 
     atualizar_tabela()
 
+def editar():
+    selecionado = tabela.selection()
+
+    if not selecionado:
+        return messagebox.showwarning("Atenção!", "Selecione um livro na lista para editar.")
+
+    indice = tabela.index(selecionado[0])
+
+    livros = database.carregar_livros()
+    livro_selecionado = livros[indice]
+
+    janela_popup_edicao = popup = ctk.CTkToplevel(janela)
+    popup.title("Editar livro")
+    popup.geometry("350x300")
+    popup.grab_set()
+
+    def ao_fechar_popup(): # Libera o travamento da janela principal se fechar no "X"
+        popup.grab_release()
+        popup.destroy()
+        
+    popup.protocol("WM_DELETE_WINDOW", ao_fechar_popup)
+
+    ctk.CTkLabel(popup, text="Título:").pack(anchor="w", padx=20, pady=(20, 0))
+    titulo_entry_popup = ctk.CTkEntry(popup)
+    titulo_entry_popup.pack(fill="x", padx=20)
+    titulo_entry_popup.insert(0, livro_selecionado["título"])
+ 
+    ctk.CTkLabel(popup, text="Autor:").pack(anchor="w", padx=20, pady=(10, 0))
+    autor_entry_popup = ctk.CTkEntry(popup)
+    autor_entry_popup.pack(fill="x", padx=20)
+    autor_entry_popup.insert(0, livro_selecionado["autor"])
+ 
+    ctk.CTkLabel(popup, text="Ano de publicação:").pack(anchor="w", padx=20, pady=(10, 0))
+    ano_entry_popup = ctk.CTkEntry(popup)
+    ano_entry_popup.pack(fill="x", padx=20)
+    ano_entry_popup.insert(0, livro_selecionado["ano"])
+
+    # Função do botão de salvar
+    def salvar_edicao():
+        novo_titulo = titulo_entry_popup.get()
+        novo_autor = autor_entry_popup.get()
+        novo_ano = ano_entry_popup.get()
+
+        database.atualizar_livro(indice, novo_titulo, novo_autor, novo_ano)
+
+        atualizar_tabela()
+        popup.destroy()
+
+    botao_salvar = ctk.CTkButton(popup, text="Salvar", command=salvar_edicao)
+    botao_salvar.pack(pady=20)
+
+def excluir():
+    pass
+
+
 
 
 
@@ -40,12 +95,12 @@ janela.title("Cadastro de Livros")
 janela.geometry(f"850x700") # largura x altura
 janela.maxsize(1000, 780)
 janela.minsize(600, 380)
-janela.attributes("-topmost", True) # sobrepor / true ou false
+# janela.attributes("-topmost", True) # sobrepor / true ou false
 # janela.attributes("-alpha", 0.9) # transparência / entre 1 e 0
 
 # CÓDIGO ---
-titulo = ctk.CTkLabel(janela, text="Cadastre seu livro abaixo!", font=("Arial", 20, "bold"))
-titulo.pack(pady=20)
+cabecalho = ctk.CTkLabel(janela, text="Cadastre seu livro abaixo!", font=("Arial", 20, "bold"))
+cabecalho.pack(pady=20)
 
 frame_tela = ctk.CTkFrame(janela,fg_color="#B5B5B5", border_width=5, corner_radius=10)
 frame_tela.pack(side="top", fill="both", padx=35, ipadx=15, ipady=15)
@@ -93,7 +148,7 @@ frame_botao.pack(padx=35, pady=15)
 botao_adicionar = ctk.CTkButton(frame_botao, text="Adicionar", command=adicionar)
 botao_adicionar.pack(padx=5, side="left")
 
-botao_editar = ctk.CTkButton(frame_botao, text="Editar")
+botao_editar = ctk.CTkButton(frame_botao, text="Editar", command=editar)
 botao_editar.pack(padx=5, side="left")
 
 botao_excluir = ctk.CTkButton(frame_botao, text="Excluir", fg_color="#d9534f", hover_color="#c9302c")
