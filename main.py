d869cda21392551adbb2e0180a3384cb65c48996
amorimry <274507430+amorimry@users.noreply.github.com>
2026-08-24 19:35:21 -0300

    colocando novas funções

--- main.py
+++ main.py
@@ -3,6 +3,30 @@ from tkinter import ttk, messagebox
 import database
 
 # FUNÇÕES ---
+def atualizar_tabela():
+    tabela.delete(*tabela.get_children())
+
+    livros = database.carregar_livros()
+
+    for livro in livros:
+        tabela.insert("", "end", values=(livro["título"], livro["autor"], livro["ano"]))
+
+def adicionar():
+    titulo = titulo_entry.get()
+    autor = autor_entry.get()
+    ano = ano_entry.get()
+
+    if titulo == "" or autor == "" or ano == "":
+        return messagebox.showwarning("Atenção", "Você deixou algo vazio, preencha todos os campos.")
+
+    database.adicionar_livro(titulo, autor, ano)
+
+    titulo_entry.delete(0, "end")
+    autor_entry.delete(0, "end")
+    ano_entry.delete(0, "end")
+
+    atualizar_tabela()
+
 
 
 
@@ -13,7 +37,7 @@ ctk.set_appearance_mode("dark")
 # CONFIG JANELA ---
 janela = ctk.CTk()
 janela.title("Cadastro de Livros")
-janela.geometry(f"850x600") # largura x altura
+janela.geometry(f"850x700") # largura x altura
 janela.maxsize(1000, 780)
 janela.minsize(600, 380)
 janela.attributes("-topmost", True) # sobrepor / true ou false
@@ -27,11 +51,11 @@ frame_tela = ctk.CTkFrame(janela,fg_color="#B5B5B5", border_width=5, corner_radi
 frame_tela.pack(side="top", fill="both", padx=35, ipadx=15, ipady=15)
 
 
-nome_label = ctk.CTkLabel(frame_tela, text="Título", font=("Arial", 15), text_color="black", fg_color="transparent")
-nome_label.grid(row=0, column=0, sticky="w", padx=15, pady=(15, 2))
+titulo_label = ctk.CTkLabel(frame_tela, text="Título", font=("Arial", 15), text_color="black", fg_color="transparent")
+titulo_label.grid(row=0, column=0, sticky="w", padx=15, pady=(15, 2))
 
-nome_entry = ctk.CTkEntry(frame_tela, placeholder_text="Digite aqui o TÍTULO do livro...", height=35, width=400)
-nome_entry.grid(row=1, column=0, padx=15)
+titulo_entry = ctk.CTkEntry(frame_tela, placeholder_text="Digite aqui o TÍTULO do livro...", height=35, width=400)
+titulo_entry.grid(row=1, column=0, padx=15)
 
 
 autor_label = ctk.CTkLabel(frame_tela, text="Autor", font=("Arial", 15), text_color="black", fg_color="transparent")
@@ -58,15 +82,16 @@ tabela.heading("Autor", text="Autor")
 
 tabela.pack(fill="both", padx=80, pady=20)
 
+atualizar_tabela() # a função fica aq pq quando abre o arquivo desktop as informações da tabela não aparecem
+
 
 # BOTÕES ---
 frame_botao = ctk.CTkFrame(janela, fg_color="transparent")
 frame_botao.pack(padx=35, pady=15)
 
 
-botao_adicionar = ctk.CTkButton(frame_botao, text="Adicionar")
+botao_adicionar = ctk.CTkButton(frame_botao, text="Adicionar", command=adicionar)
 botao_adicionar.pack(padx=5, side="left")
-
 
 botao_editar = ctk.CTkButton(frame_botao, text="Editar")
 botao_editar.pack(padx=5, side="left")
