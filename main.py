856f511cec453158fb49a9c656c46d8cfccdf0b1
amorimry <274507430+amorimry@users.noreply.github.com>
2026-08-24 19:08:07 -0300

    tirando a transparência e ajustando a geometria

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ ctk.set_appearance_mode("dark")
 # CONFIG JANELA ---
 janela = ctk.CTk()
 janela.title("Cadastro de Livros")
-janela.geometry(f"700x600") # largura x altura
+janela.geometry(f"850x600") # largura x altura
 janela.maxsize(1000, 780)
 janela.minsize(600, 380)
 janela.attributes("-topmost", True) # sobrepor / true ou false
-janela.attributes("-alpha", 0.9) # transparência / entre 1 e 0
+# janela.attributes("-alpha", 0.9) # transparência / entre 1 e 0
 
 # CÓDIGO ---
 titulo = ctk.CTkLabel(janela, text="Cadastre seu livro abaixo!", font=("Arial", 20, "bold"))
